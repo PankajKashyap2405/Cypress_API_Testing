4d44fa57032ed6464aec8f2bcb98d14d5a0f7aa0
Pankaj Surya <pankajkashyap2405@gmail.com>
2024-03-09 01:47:20 +0530

    lenght

--- cypress/e2e/getResponse1.cy.js
+++ cypress/e2e/getResponse1.cy.js
@@ -5,6 +5,7 @@ describe('template spec', () => {
     cy.request('GET','https://www.bstackdemo.com/').then((response) =>{
     //Expecting the response status code to be 200
     expect(response.status).to.eq(200)
+    expect(body.length).to.be.greaterThan(1)
       })
    })
 })
